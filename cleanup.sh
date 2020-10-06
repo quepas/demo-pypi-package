305691b22f584eb1e11aeaa792a5be27010f99a4
quepas <kiepas.patryk@gmail.com>
2020-10-06 18:47:10 +0200

    Correcting the package version

---
+++ cleanup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+rm demo_pypi_prackage.egg-info/ -rf
+rm build/ -rf
+rm dist/ -rf
+
--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="demo-pypi-prackage", # Replace with your own username
-    version="0.0.2",
+    name="demo-pypi-package", # Replace with your own username
+    version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
