763e8d691b09e2dab65f0ff0a08974089a49fcc5
quepas <kiepas.patryk@gmail.com>
2020-10-08 23:21:05 +0200

    Updating the cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-rm demo_pypi_prackage.egg-info/ -rf
+rm CMakeFiles/ -rf
+rm demo_pypi_package.egg-info/ -rf
 rm build/ -rf
 rm dist/ -rf
-
+rm lib/ -rf
+rm cmake_install.cmake
+rm CMakeCache.txt
+rm Makefile
