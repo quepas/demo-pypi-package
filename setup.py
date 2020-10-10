bb39386f5f35d4f721cd75a7afe115cc7321be76
quepas <kiepas.patryk@gmail.com>
2020-10-10 10:06:04 +0200

    Improving the cmake integration; adding the python data

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import setuptools
 from setuptools import Extension
 from setuptools.command.build_ext import build_ext
+from distutils.command.install_data import install_data
+from setuptools.command.install_lib import install_lib
+import os, shutil
 
 class CMakeExtension(Extension):
     """
@@ -11,8 +14,94 @@ class CMakeExtension(Extension):
     """
 
     def __init__(self, name, sources=[]):
-
         super().__init__(name = name, sources = sources)
+
+class InstallCMakeLibsData(install_data):
+    """
+    Just a wrapper to get the install data into the egg-info
+
+    Listing the installed files in the egg-info guarantees that
+    all of the package files will be uninstalled when the user
+    uninstalls your package through pip
+    """
+
+    def run(self):
+        """
+        Outfiles are the libraries that were built using cmake
+        """
+
+        # There seems to be no other way to do this; I tried listing the
+        # libraries during the execution of the InstallCMakeLibs.run() but
+        # setuptools never tracked them, seems like setuptools wants to
+        # track the libraries through package data more than anything...
+        # help would be appriciated
+
+        self.outfiles = self.distribution.data_files
+
+class InstallCMakeLibs(install_lib):
+    """
+    Get the libraries from the parent distribution, use those as the outfiles
+
+    Skip building anything; everything is already built, forward libraries to
+    the installation step
+    """
+
+    def run(self):
+        """
+        Copy libraries from the bin directory and place them as appropriate
+        """
+
+        self.announce("Moving library files", level=3)
+
+        # We have already built the libraries in the previous build_ext step
+
+        self.skip_build = True
+
+        bin_dir = './lib'#self.distribution.bin_dir
+
+        # Depending on the files that are generated from your cmake
+        # build chain, you may need to change the below code, such that
+        # your files are moved to the appropriate location when the installation
+        # is run
+
+        libs = [os.path.join(bin_dir, _lib) for _lib in 
+                os.listdir(bin_dir) if 
+                os.path.isfile(os.path.join(bin_dir, _lib)) and 
+                os.path.splitext(_lib)[1] in [".dll", ".so"]
+                and not (_lib.startswith("python") or _lib.startswith('example_pkg'))]
+
+        for lib in libs:
+
+            shutil.move(lib, os.path.join(self.build_dir,
+                                          os.path.basename(lib)))
+
+        # Mark the libs for installation, adding them to 
+        # distribution.data_files seems to ensure that setuptools' record 
+        # writer appends them to installed-files.txt in the package's egg-info
+        #
+        # Also tried adding the libraries to the distribution.libraries list, 
+        # but that never seemed to add them to the installed-files.txt in the 
+        # egg-info, and the online recommendation seems to be adding libraries 
+        # into eager_resources in the call to setup(), which I think puts them 
+        # in data_files anyways. 
+        # 
+        # What is the best way?
+
+        # These are the additional installation files that should be
+        # included in the package, but are resultant of the cmake build
+        # step; depending on the files that are generated from your cmake
+        # build chain, you may need to modify the below code
+
+        self.distribution.data_files = [os.path.join(self.install_dir, 
+                                                     os.path.basename(lib))
+                                        for lib in libs]
+
+        # Must be forced to run after adding the libs to data_files
+
+        self.distribution.run_command("install_data")
+
+        super().run()
+
 
 class BuildCMakeExt(build_ext):
     """
@@ -26,7 +115,7 @@ class BuildCMakeExt(build_ext):
 
         for extension in self.extensions:
 
-            if extension.name == 'example_extension':
+            if extension.name == 'peptalk':
 
                 self.build_cmake(extension)
 
@@ -57,7 +146,7 @@ class BuildCMakeExt(build_ext):
         self.spawn(['cmake', 'CMakeLists.txt'])
 
         self.announce("Building binaries", level=3)
-        self.spawn(['make', '-j', '4'])
+        self.spawn(['cmake', '--build', '.'])
 
         # Build finished, now copy the files into the copy directory
         # The copy directory is the parent directory of the extension (.pyd)
@@ -106,6 +195,8 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    ext_modules=[CMakeExtension('example_extension')],
-    cmdclass={'build_ext': BuildCMakeExt}
+    ext_modules=[CMakeExtension('peptalk')],
+    cmdclass={'build_ext': BuildCMakeExt,
+    'install_data': InstallCMakeLibsData,
+    'install_lib': InstallCMakeLibs}
 )
